b1c2e2558a3f0cd9a16f850b364cf2def294aa78
Rotem <rotemls98@gmail.com>
2020-06-05 11:28:31 +0300

    fix

--- src/forms/v2/useForm.js
+++ src/forms/v2/useForm.js
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import { isEmpty } from "../utils/utils";
 
-const useForm = ({initialValues = {}, validate}) => {
+const useForm = ({ initialValues = {}, validate }) => {
     const [values, setValues] = useState(initialValues);
     const [touched, setTouched] = useState({});
 
@@ -11,31 +11,30 @@ const useForm = ({initialValues = {}, validate}) => {
         const getValue = () => {
             if (type === 'checkbox') {
                 return e.target.checked;
-            }
-            else if (type === 'select-multiple') {
+            } else if (type === 'select-multiple') {
                 return Array.from(e.target.selectedOptions).map(o => o.value);
             }
             return e.target.value;
         }
 
         const value = getValue();
-        setValues(prevValues => ({...prevValues, [name]: value}));
+        setValues(prevValues => ({ ...prevValues, [name]: value }));
     };
 
     const handleBlur = (e) => {
         const { name } = e.target;
-        setTouched(prevTouched => ({...prevTouched, [name]: true}));
+        setTouched(prevTouched => ({ ...prevTouched, [name]: true }));
     };
 
     const errors = validate ? validate(values) : {};
     const valid = isEmpty(errors);
 
     const handleSubmit = (onSubmit) => {
-        if (valid) {
-            return (e) => {
-                if (e && typeof e.preventDefault === 'function') {
-                    e.preventDefault();
-                }
+        return (e) => {
+            if (e && typeof e.preventDefault === 'function') {
+                e.preventDefault();
+            }
+            if (valid) {
                 onSubmit(values, e);
             }
         }
